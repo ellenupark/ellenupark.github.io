ec97496af64c5b2e3dac374ffbb7f6f77f68d351
ellenupark <ellenupark@gmail.com>
2021-02-03 17:30:43 -0500

    Updated blog post url to include updates

---
+++ src/components/BlogPost.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+
+const BlogPost = (props) => {
+    const formatDate = (date) => {
+        const dateArray = date.split(' ')[0];
+        const year = dateArray.split('-')[0].toString();
+        const month = dateArray.split('-')[1].toString();
+        const day = dateArray.split('-')[2].toString();
+
+        const monthNames = {
+          '01': "JANUARY", '02': "FEBRUARY", '03': "MARCH",
+          '04': "APRIL", '05': "MAY", '06': "JUNE", '07': "JULY",
+          '08': "AUGUST", '09': "SEPTEMBER", '10': "OCTOBER",
+          '11': "NOVEMBER", '12': "DECEMBER"
+        };
+
+        return `${monthNames[month]} ${day}, ${year}`
+    }
+      
+
+    return (
+       <>
+        <div className="card">
+            <img src={props.article.thumbnail} alt="Blog Post"/>
+            <h3>{props.article.title}</h3>
+            <p>{formatDate(props.article.pubDate)}</p>
+            <div className='categories'>
+                {props.article.categories.map(category => <span>{category}</span>)}
+            </div>
+        </div>
+       </>
+    );
+};
+
+export default BlogPost;
--- src/containers/BlogContainer.js
+++ src/containers/BlogContainer.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import BlogPost from '../components/BlogPost'
+import axios from 'axios';
 
 const BlogContainer = () => {
+    const [articles, setArticles] = useState([])
+
+    useEffect(() => {
+        retrieveRecentArticles();
+    }, [])
+
+    const retrieveRecentArticles = () => {
+        return axios
+            .get('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@ellen-park?t=1612391274')
+            .then(({data}) => data)
+            .then(posts => {
+                setArticles(posts.items.slice(0, 7))
+            })
+    }
+
     return (
         <div className='blog'>
             <h2>Blog</h2>
+            <div className="cards">
+                {articles.map(article => <a href={article.link}><BlogPost article={article}/></a>)}
+                {/* <div className="card">ONE</div>
+                <div className="card">TWO</div>
+                <div className="card">THREE</div>
+                <div className="card">FOUR</div>
+                <div className="card">FIVE</div>
+                <div className="card">SIX</div> */}
+            </div>
         </div>
     );
 };
